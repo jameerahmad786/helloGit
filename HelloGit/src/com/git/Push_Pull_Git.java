ec82552d2c989c2fea192696fdc08f975ca0932e
jameerahmad786 <jameer.786ahmad@gmail.com>
2017-03-01 22:42:11 +0530

    Added Push_Pull_Git.java

---
+++ HelloGit/src/com/git/Push_Pull_Git.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class Push_Pull_Git {
+public static void main(String[] args) {
+	String games[]={"Cricket","Hockey","Football"};
+	for(int i=0;i<games.length;i++)
+	{
+		System.out.println(games[i]);
+	}
+}
+}
