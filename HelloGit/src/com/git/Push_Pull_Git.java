49003093975a45ac6fe01c4b68f8bfd769acb956
839147 <839147@01HW805289.TcsAdobeOdc.com>
2017-03-02 01:30:59 +0530

    added one more print out and two array elements

--- HelloGit/src/com/git/Push_Pull_Git.java
+++ HelloGit/src/com/git/Push_Pull_Git.java
@@ -2,10 +2,11 @@ package com.git;
 
 public class Push_Pull_Git {
 public static void main(String[] args) {
-	String games[]={"Cricket","Hockey","Football"};
+	String games[]={"Cricket","Hockey","Football","tennis","volleyball"};
 	for(int i=0;i<games.length;i++)
 	{
 		System.out.println(games[i]);
+		System.out.println("How are you doing today.");
 	}
 }
 }
